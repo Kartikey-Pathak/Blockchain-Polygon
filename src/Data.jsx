6c4e7fca3afdacb05179fad268b8b8e4811ca54c
Kartikey <kartikeypathak08@gmail.com>
2025-07-04 21:30:51 +0530

    Fixed Some bugs

--- src/Data.jsx
+++ src/Data.jsx
@@ -7,25 +7,20 @@ function Data() {
     const [low, high] = useState()
     const coin = location.state;
     return (
-        <div className=" w-full max-w-screen md:min-h-screen h-[90rem] relative bg-black">
+        <div className=" w-full max-w-screen md:min-h-screen h-[90rem] md:h-[70rem] relative bg-black">
             <header>
                 <Nav />
             </header>
 
             <main>
                 <div className="w-ful max-w-screen h-[35rem] flex items-center justify-center">
-                    <div className="mt-50 flex items-center justify-center w-full h-full md:flex-row flex-col ">
-                        {/* graph */}
-                        <div className="md:w-[90%] w-[80%] h-[40vh]  md:order-1 flex items-center justify-center rounded-4xl  bg-white">
-                            f
-
-
-                        </div>
+                    <div className="mt-50 flex items-center justify-center w-full h-full flex-col lg:flex-row ">
+                        
 
                         {/* information */}
-                        <div className=" h-full md:w-full flex items-start flex-col md:overflow-y-scroll md:order-2">
+                        <div className=" h-full md:w-full flex items-start flex-col border-2 md:overflow-y-scroll md:order-2">
                             <h1 className="text-4xl text-white font-semibold mt-3 mx-auto">Coin Info</h1>
-                            <div className=" h-20 w-[38vw]  md:w-[17vw]  m-10 flex flex-col">
+                            <div className=" h-20 w-[60vw]  md:w-[30vw] items-center justify-center  m-10 flex flex-col">
                                 <div className="h-1/2 w-full flex items-center justify-start flex-row">
 
                                     <div className="w-1/4  h-full flex items-start gap-5 justify-start">
@@ -37,7 +32,7 @@ function Data() {
 
                                 <div className="h-1/2 w-full flex items-center justify-center">
                                     <div className=" h-full w-full flex items-center justify-center gap-5">
-                                        <h1 className="text-white text-3xl font-semibold">₹ {coin.current_price.toLocaleString('en-IN')}</h1>
+                                        <h1 className="text-white text-3xl font-semibold">₹{coin.current_price.toLocaleString('en-IN')}</h1>
                                         <h1 className=" text-green-700 text-xl">{coin.price_change_percentage_24h.toFixed(2)}</h1>
                                     </div>
                                 </div>
@@ -49,7 +44,7 @@ function Data() {
                             <div className=' w-full flex justify-center flex-col gap-10 items-center'>
                             <div className=' w-[80%] md:w-[70%] px-5 border-b-2 border-gray-600 flex items-start gap-5 flex-row justify-between'>
                                 <h1 className=' text-gray-400 text-xl md:text-2xl font-semibold'>Market Cap</h1>
-                                <h1 className='text-gray-400 text-xl md:text-2xl font-semibold'>₹ {coin.market_cap.toLocaleString('en-IN')}</h1>
+                                <h1 className='text-gray-400 text-xl md:text-2xl font-semibold'>₹{coin.market_cap.toLocaleString('en-IN')}</h1>
                             </div>
                             <div className=' w-[80%] md:w-[70%] px-5 border-b-2 border-gray-600 flex items-start gap-5 flex-row justify-between'>
                                 <h1 className=' text-gray-400 text-xl  md:text-2xl font-semibold'>Fully Diluted Valuation</h1>
@@ -83,10 +78,17 @@ function Data() {
 
 
                         </div>
+
+                        {/* graph */}
+                        <div className="md:w-[90%] w-[90%] h-[16rem] order-2 flex items-center justify-center rounded-4xl  bg-white">
+                            f
+
+
+                        </div>
+
+
                     </div>
-
                 </div>
-
             </main>
 
 
