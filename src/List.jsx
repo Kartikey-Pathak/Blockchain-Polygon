36db8663f6585c70fa2e05279f92f0b013ea8f6a
Kartikey <kartikeypathak08@gmail.com>
2025-07-13 23:52:12 +0530

    Added Ui Loader And Alingned it accordingy for small decvices

--- src/List.jsx
+++ src/List.jsx
@@ -3,8 +3,26 @@ import { div } from "framer-motion/client";
 function List() {
     return (
         <div className=" w-full max-w-screen h-full bg-black">
-            <div className=" flex items-center justify-center h-screen">
-                <h1 className=" text-6xl text-warning font-semibold ">Nothing To Show Currently...</h1>
+            <div className=" flex items-center justify-center h-full md:h-screen md:flex-row flex-col">
+                
+              <div className="flex w-52 flex-col gap-4 m-10">
+                    <div className="skeleton h-32 w-full"></div>
+                    <div className="skeleton h-4 w-28"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                </div>
+                <div className="flex w-52 flex-col gap-4 m-10">
+                    <div className="skeleton h-32 w-full"></div>
+                    <div className="skeleton h-4 w-28"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                </div>
+                <div className="flex w-52 flex-col gap-4 m-10">
+                    <div className="skeleton h-32 w-full"></div>
+                    <div className="skeleton h-4 w-28"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                    <div className="skeleton h-4 w-full"></div>
+                </div>
 
                 <div className="flex w-52 flex-col gap-4 m-10">
                     <div className="skeleton h-32 w-full"></div>
