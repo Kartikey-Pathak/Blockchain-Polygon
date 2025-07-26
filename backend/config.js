efd04948844d2a310cdf444d256552f8a133884f
Kartikey <kartikeypathak08@gmail.com>
2025-07-26 12:24:23 +0530

    Hosting changes

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,4 @@ dotenv.config();
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGO_URI);
